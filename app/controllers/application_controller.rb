0aad33d2d2514699d000138e765d22b8d6d0c99f
Sean Martin <sean@seanmartinstudio.com>
2022-06-22 12:24:56 -0700

    commit tree 81a8d8ed41f7c5ebaa5b3abeaa506c9118a5dc53

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
@@ -22,5 +24,29 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+
+  post '/baked_goods' do
+   baked_good = BakedGood.create(
+    name: params[:name],
+    price: params[:price],
+    bakery_id: params[:bakery_id]
+   )
+   baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.delete
+    baked_good.to_json
+  end
+
 
 end
